26999efbe44651f32aa7a77a87c26b0ee7f4fa4a
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-10-17 13:53:58 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import os
-from sqlalchemy import create_engine, Column, String, Integer 
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Criando banco de dados
+# Criando banco de dados.
 MEU_BANCO = create_engine("sqlite:///meubanco.db")
 
 # Criando conexão com banco de dados.
@@ -11,6 +11,7 @@ session = Session()
 
 
 # Criando tabela.
+
 Base = declarative_base()
 
 class Usuario(Base):
@@ -19,11 +20,11 @@ class Usuario(Base):
     # Definindo campos da tabela.
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     nome = Column("nome", String)
-    email = Column("emai", String)
+    email = Column("email", String)
     senha = Column("senha", String)
 
     # Definindo atributos da classe.
-    def __init__(self, nome: str, email:str, senha:str):
+    def __init__(self, nome: str, email: str, senha: str):
         self.nome = nome
         self.email = email
         self.senha = senha
@@ -34,55 +35,60 @@ Base.metadata.create_all(bind=MEU_BANCO)
 # Salvar no banco de dados.
 os.system("cls || clear")
 
+# Create
 print("Solicitando dados para o usuário")
-insirir_nome = input("Digite seu nome: ")
-insirir_email = input("Digite seu e-mail: ")
-insirir_senha = input("Digite sua senha: ")
+inserir_nome = input("Digite seu nome: ")
+inserir_email = input("Digite seu e-mail: ")
+inserir_senha = input("Digite seu senha ")
 
-usuario = Usuario(nome=insirir_nome, email=insirir_email, senha=insirir_senha)
+usuario = Usuario(nome=inserir_nome, email=inserir_email, senha=inserir_senha)
 session.add(usuario)
 session.commit()
 
 # Listando todos os usuários do banco de dados.
-print("\nExibindo todos os usuários do banco de dados.")
+# Read
+print("\nExibindo todos os usuários do bando de dados.")
 lista_usuarios = session.query(Usuario).all()
 
-# Read
 for usuario in lista_usuarios:
-    print(f"{usuario.id} - Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+    print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
 
 # Delete
 print("\nExcluindo um usuário.")
-email_usuario = input("Informe o email do usuário para ser excluído: ")
+email_usuario = input("Informe o email do usuário para ser excluido: ")
 
 usuario = session.query(Usuario).filter_by(email = email_usuario).first()
 session.delete(usuario)
 session.commit()
-
-os.system("cls || clear")
-print(f"{usuario.nome} excluído com sucesso.")
+print(f"{usuario.nome} excluido com sucesso.")
 
 # Listando todos os usuários do banco de dados.
-print("\nExibindo todos os usuários do banco de dados.")
+# Read
+print("\nExibindo todos os usuários do bando de dados.")
 lista_usuarios = session.query(Usuario).all()
 
-# Read
 for usuario in lista_usuarios:
-    print(f"{usuario.id} - Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+    print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
 
 # Update
 print("\nAtualizando dados do usuário.")
+email_usuario = input("Informe o email do usuário que será atualizado: ")
+
 usuario = session.query(Usuario).filter_by(email = email_usuario).first()
 
-novos_dados = Usuario(
-    nome = input("Digite seu nome: "),
-    email= input("Digite seu e-mail: "),
-    senha = input("Digite sua senha: ")
-)
+usuario.nome = input("Digite seu nome: ")
+usuario.email = input("Digite seu e-mail: ")
+usuario.senha = input("Digite seu senha: ")
 
-usuario = novos_dados
-session.add(usuario)
 session.commit()
 
-# Fechando conexão.    
+# Listando todos os usuários do banco de dados.
+# Read
+print("\nExibindo todos os usuários do bando de dados.")
+lista_usuarios = session.query(Usuario).all()
+
+for usuario in lista_usuarios:
+    print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
+
+# Fechando conexão.
 session.close()
